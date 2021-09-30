326fa71ae0074039369090376b19231469dc436e
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-09-30 14:51:39 -0300

    pages

---
+++ app/screens/Restaurants.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Restaurants = () => {
+    return (
+      <View>
+        <Text>Restaurants Restaurants</Text>
+      </View>
+    );
+}
+
+export default Restaurants
---
+++ app/screens/TopRestaurans.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const TopRestaurans = () => {
+    return (
+      <View>
+        <Text>TopRestaurans TopRestaurans</Text>
+      </View>
+    );
+}
+
+export default TopRestaurans
