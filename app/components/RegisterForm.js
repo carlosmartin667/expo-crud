5fc3740a1553da2c981413b1f5877df801199faa
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-10-01 16:14:43 -0300

    register forms

---
+++ app/components/RegisterForm.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Button, Input } from "react-native-elements";
+
+const RegisterForm = () => {
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        placeholder="correo electronico"
+        containerStyle={styles.inputForm}
+      />
+      <Input
+        placeholder="contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={true}
+      />
+      <Input
+        placeholder="reoetir contraseña"
+        containerStyle={styles.inputForm}
+        password={true}
+        secureTextEntry={true}
+      />
+      <Button
+        title="Unirse"
+        containerStyle={styles.btnContainerRegister}
+        buttonStyle={styles.btnRegister}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  inputForm: {
+    width: "100%",
+    marginTop: 20,
+  },
+  btnContainerRegister: {
+    marginTop: 20,
+    width: "95%",
+  },
+  btnRegister: {
+    backgroundColor: "#00a680",
+  },
+  iconRight: {
+    color: "#c1c1c1",
+  },
+});
+export default RegisterForm;
--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import RegisterForm from "../../components/RegisterForm";
 
 const Register = () => {
-    return (
-      <View>
-        <Text>Register</Text>
+  return (
+    <View>
+      <Image
+        source={require("../../../assets/img/7.1 5-tenedores-letras-icono-logo.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <View style={styles.viewForm}>
+        <RegisterForm />
       </View>
-    );
-}
-
-export default Register
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+});
+export default Register;
