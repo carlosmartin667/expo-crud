d54f5ad0a7a8c2c38f17f8531703b94ecfb65d74
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-10-01 12:20:44 -0300

    creando stack y pantalla de login

--- app/routers/AccountStack.js
+++ app/routers/AccountStack.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Restaurants from "../screens/Restaurants";
 import Account from "../screens/Account/Account";
+import Login from "../screens/Account/Login";
 
 
 const Stack = createNativeStackNavigator();
@@ -10,9 +11,14 @@ const AccountStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="Account Stack"
+        name="AccountStack"
         component={Account}
         options={{ title: "Account Stack " }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: "iniciar seccion " }}
       />
     </Stack.Navigator>
   );
