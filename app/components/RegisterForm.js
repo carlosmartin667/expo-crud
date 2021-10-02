85a3d0c8c18d68cfdbd4baece3dc829d852aa496
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-10-02 11:19:10 -0300

    cambios e reireccion

--- app/components/RegisterForm.js
+++ app/components/RegisterForm.js
@@ -37,7 +37,7 @@ const RegisterForm = (props) => {
         .createUserWithEmailAndPassword(formData.email, formData.password)
         .then((response) => {
           setLoading(false);
-          navigation.navigate("Account");
+          navigation.navigate("AccountStack");
           console.log(response);
         })
         .catch(() => {
