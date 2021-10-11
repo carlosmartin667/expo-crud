90f2cd08ab4d401cfb336cb8322212a7d90569ee
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-10-11 11:56:18 -0300

    configurano ruta para add restaurantes

--- app/routers/RestaurantsStack.js
+++ app/routers/RestaurantsStack.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Restaurants from "../screens/Restaurants/Restaurants";
-
+import AddRestaurant from "../screens/Restaurants/AddRestaurant";
 
 const Stack = createNativeStackNavigator();
 const RestaurantsStack = () => {
-    return (
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Restaurants"
-          component={Restaurants}
-          options={{ title: "restaurantes " }}
-        />
-      </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Restaurants"
+        component={Restaurants}
+        options={{ title: "restaurantes " }}
+      />
+      <Stack.Screen
+        name="add-restaurant"
+        component={AddRestaurant}
+        options={{ title: "añadir nuevo restaurante " }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-export default RestaurantsStack
+export default RestaurantsStack;
---
+++ app/screens/Restaurants/AddRestaurant.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const AddRestaurant = () => {
+    return (
+      <View>
+        <Text>AddRestaurant</Text>
+      </View>
+    );
+}
+
+export default AddRestaurant
