f4b6e09ced9e865b972068b59c326e3b85f276c3
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-10-11 12:15:10 -0300

    formulario agregar restaurante

---
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Alert,
+  Dimensions,
+} from "react-native";
+import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+
+const AddRestaurantForm = (props) => {
+  const { toastRef, setIsLoading, navigation } = props;
+  const [restaurantName, setRestaurantName] = useState("");
+  const [restaurantAddress, setRestaurantAddress] = useState("");
+  const [restaurantDescription, setRestaurantDescription] = useState("");
+  const [imagesSelected, setImagesSelected] = useState([]);
+  const [isVisibleMap, setIsVisibleMap] = useState(false);
+  const [locationRestaurant, setLocationRestaurant] = useState(null);
+  return (
+    <ScrollView style={styles.scrollView}>
+      <FormAdd />
+    </ScrollView>
+  );
+};
+const FormAdd = (props) => {
+  const {
+    setRestaurantName,
+    setRestaurantAddress,
+    setRestaurantDescription,
+    setIsVisibleMap,
+    locationRestaurant,
+  } = props;
+
+  return (
+    <View style={styles.viewForm}>
+      <Input
+        placeholder="Nombre del restaurante"
+        containerStyle={styles.input}
+        onChange={(e) => setRestaurantName(e.nativeEvent.text)}
+      />
+      <Input
+        placeholder="Dirección"
+        containerStyle={styles.input}
+        onChange={(e) => setRestaurantAddress(e.nativeEvent.text)}
+        rightIcon={{
+          type: "material-community",
+          name: "google-maps",
+          color: locationRestaurant ? "#00a680" : "#c2c2c2",
+          onPress: () => setIsVisibleMap(true),
+        }}
+      />
+      <Input
+        placeholder="Descripcion del restaurante"
+        multiline={true}
+        inputContainerStyle={styles.textArea}
+        onChange={(e) => setRestaurantDescription(e.nativeEvent.text)}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  scrollView: {
+    height: "100%",
+  },
+  viewForm: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  btnAddRestaurant: {
+    backgroundColor: "#00a680",
+    margin: 20,
+  },
+  viewImages: {
+    flexDirection: "row",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 30,
+  },
+  containerIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+    height: 70,
+    width: 70,
+    backgroundColor: "#e3e3e3",
+  },
+  miniatureStyle: {
+    width: 70,
+    height: 70,
+    marginRight: 10,
+  },
+  viewPhoto: {
+    alignItems: "center",
+    height: 200,
+    marginBottom: 20,
+  },
+  mapStyle: {
+    width: "100%",
+    height: 550,
+  },
+  viewMapBtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  viewMapBtnContainerCancel: {
+    paddingLeft: 5,
+  },
+  viewMapBtnCancel: {
+    backgroundColor: "#a60d0d",
+  },
+  viewMapBtnContainerSave: {
+    paddingRight: 5,
+  },
+  viewMapBtnSave: {
+    backgroundColor: "#00a680",
+  },
+});
+export default AddRestaurantForm;
--- app/screens/Restaurants/AddRestaurant.js
+++ app/screens/Restaurants/AddRestaurant.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useRef, useState } from "react";
+import { View } from "react-native";
+// import Toast from "react-native-easy-toast";
+import Loading from "../../components/Account/Loading";
+import AddRestaurantForm from "../../components/Restaurants/AddRestaurantForm";
 
-const AddRestaurant = () => {
-    return (
-      <View>
-        <Text>AddRestaurant</Text>
-      </View>
-    );
-}
+const AddRestaurant = (props) => {
+  const { navigation } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const toastRef = useRef();
+  return (
+    <View>
+      <AddRestaurantForm
+        toastRef={toastRef}
+        setIsLoading={setIsLoading}
+        navigation={navigation}
+      />
+      <Loading isVisible={isLoading} text="Creando restaurante" />
+    </View>
+  );
+};
 
-export default AddRestaurant
+export default AddRestaurant;
